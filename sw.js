d5dbdb40fc1f4cd3a4fb5f49dabd2f6f3ade8e19
riyasaalabs <riyasaa.rabbit@gmail.com>
2019-01-26 14:06:10 +0800

    first commit

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "tms";
+const CACHE_VERSION = 1;
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME + CACHE_VERSION.toString()).then(function(cache) {
+      return cache.addAll([
+        'index.ejs',
+        'sw.js',
+        './index.js',
+      ]);
+    })
+  );
+});
+
+this.addEventListener('fetch', event => {
+  //Retrieval from cache
+  event.respondWith(caches.match(event.request).then(function(response) {
+    if (response !== undefined) {  // Found match in cache
+      return response;
+   } else {
+      return fetch(event.request).then(function (response) {
+          //Response may be used only once so need to clone it for reuse
+          let responseClone = response.clone();
+          caches.open(CACHE_VERSION.toString()).then(function (cache) {
+             cache.put(event.request, responseClone);
+          });
+          return response;
+      });
+   }
+ }));
+});
